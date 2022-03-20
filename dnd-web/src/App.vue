b3949b77a13bcc3454d9b5a6f6a47eb17d4f46ed
Zuvix <samuelkubala@gmail.com>
2022-03-21 00:09:39 +0100

    Vue create app

---
+++ dnd-web/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, computed } from "vue";
+const questions = ref([
+  {
+    question: "Select a race for your character",
+  },
+]);
+</script>
+
+<template>
+  <h1>DND Editor</h1>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Montserrat", sans-serif;
+  background-color: #271c36;
+  color: #fff;
+}
+</style>
